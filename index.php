017211715a32812da16420d61160b4e485c7024b
Ayush Ranjan Rout Rout <57044707+AyushRanjanRout@users.noreply.github.com>
2023-10-16 09:30:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
-if(isset($_POST['text'])){
-    $input = $_POST['text'];
-    $output = base64_decode($input);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['text'])) {
+        $input = $_POST['text'];
+        $output = base64_decode($input);
+    }
+}
+
+function safelyOutput($data) {
+    return htmlentities($data, ENT_QUOTES, 'UTF-8');
 }
 ?>
 
@@ -20,10 +26,9 @@ if(isset($_POST['text'])){
 <body>
     <h1 style="text-align: center;">Base64 Decoder</h1>
     <form action="decode.php" class="decode_form" method="post">
-        <input type="text" name="text" class="decode_input" placeholder="Enter Your Text"/>
+        <input type="text" name="text" class="decode_input" placeholder="Enter Your Text" value="<?php echo isset($_POST['text']) ? safelyOutput($_POST['text']) : ''; ?>"/>
         <button type="submit" class="decode_button">Decode</button>
     </form>
-    <textarea id="" cols="30" class="decode_output" rows="10" ><?php if(isset($output)){echo $output;} ?></textarea>
-
+    <textarea id="" cols="30" class="decode_output" rows="10" ><?php echo isset($output) ? safelyOutput($output) : ''; ?></textarea>
 </body>
 </html>
